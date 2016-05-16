8a601e4d1aa9380a1df2f803c48a50a4a0400298
PeteAJ <peter.jacobson@gmail.com>
2016-05-15 20:46:29 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515225523) do
+ActiveRecord::Schema.define(version: 0) do
 
 end
