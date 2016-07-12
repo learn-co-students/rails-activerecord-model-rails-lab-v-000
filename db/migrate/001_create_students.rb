6839a60d5c5f7d77225c2a2619da5d4ae462f847
wook kim <wooke2e@gmail.com>
2016-07-12 18:49:39 +0000

    Done.

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,6 +1,6 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :studnets do |t|
+    create_table :students do |t|
       t.string :first_name
       t.string :last_name
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "studnets", force: :cascade do |t|
+  create_table "students", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.datetime "created_at", null: false
