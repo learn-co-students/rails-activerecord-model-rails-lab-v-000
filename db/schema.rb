8f9dcb7d3c59538436ab145b6780fe1d8aa47901
Adam Taitano <adam.taitano@gmail.com>
2016-07-29 19:40:36 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,9 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
