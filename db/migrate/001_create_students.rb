ddc735752d7f4b9347d4c66e8743f968bee0db62
ode-to-the-code <justcallmethis@gmail.com>
2017-06-22 23:08:30 -0400

    Done.

---
+++ db/migrate/001_create_students.rb
@@ -0,0 +1,13 @@
+class CreateStudents < ActiveRecord::Migration
+
+  def change
+      create_table  :students do |t|
+        t.string :first_name
+        t.string :last_name
+
+        t.timestamps null: false
+
+      end
+  end
+
+end
