e883cfd0cdf612f334e327a92f9c0476c9aab711
Caroline Shields <cbshields@outlook.com>
2018-02-14 17:47:44 +0000

    created Student model

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,6 +1,6 @@
-class CreateStudents < Application::Migration
+class CreateStudents < ActiveRecord::Migration
 
-create_table students do |t|
+create_table :students do |t|
   t.string :first_name
   t.string :last_name
 end
