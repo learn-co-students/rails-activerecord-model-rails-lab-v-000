ec5502333891d55badce436a2a5565c66a5a5f93
Kaitlin Kelly <kk504408@gmail.com>
2018-03-12 23:52:55 +0000

    Done.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#  pending "add some examples to (or delete) #{__FILE__}"
+  it "can create an instance of the Student class" do
+    @student = Student.create(first_name: "Kait", last_name: "Kelly")
+    @students = Student.all
+    expect(@students).to include(@student)
+  end
+
+  it "an instance method can respond to method to_s" do
+    expect(@student).to respond_to(:to_s)
+  end
 end
