d3257465d5023ec3aa628cd07b2fb6ac618bd2fd
Susan Fowler <infopro94@gmail.com>
2018-03-19 16:47:41 -0500

    finish instance method in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,7 @@
 class Student < ActiveRecord::Base
 
-	def initialize(student)
-		@student = student
+	def to_s
+		@student = "#{first_name}" + ' ' + "#{last_name}"
 	end
-
-	def to_s(first_name, last_name)
-		student = Student.new
-		student.to_s("#{student.first_name} + #{student.last_name}")
-	end
-
 
 end 
