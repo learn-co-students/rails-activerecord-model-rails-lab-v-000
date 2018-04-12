276c5f66ef2b7c619315698932bd776bb3c675b2
churchwerks <steven.lennis@icloud.com>
2018-04-12 11:19:03 -0500

    Done.

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Student do
+  it 'can be created' do
+    student = Student.create!(first_name: "My title", last_name: "The post description")
+    expect(student).to be_valid
+  end
 end
