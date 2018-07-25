659450c5a663677dda0fe38c362e5efea2a70822
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-25 18:37:27 +0000

    Done.

--- db/migrate/20180725183020_create_students.rb
+++ db/migrate/20180725183020_create_students.rb
@@ -1,8 +1,11 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
+<<<<<<< HEAD
       t.string :first_name
       t.string :last_name
+=======
+>>>>>>> 39e95a652b44f0f66d4512b01508e792f37d56fc
     end
   end
 end
