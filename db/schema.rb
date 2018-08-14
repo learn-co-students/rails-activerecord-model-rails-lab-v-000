9ade2a428bc1304fff75d1646043ebfb99d4b7f7
Jed Peek <jedpeek@yahoo.com>
2018-08-14 19:02:15 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180814185202) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
 end
