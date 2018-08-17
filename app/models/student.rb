15c85160d0734485151dd968a5d06318fa251107
NeedsAUsername <kevinchi118@gmail.com>
2018-08-17 12:25:19 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,10 @@
 class Student < ActiveRecord::Base
 
+  def shout
+    "Hey!"
+  end 
+
   def to_s
     "#{first_name} #{last_name}"
-  end 
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can be created' do
+    student = Student.create(first_name: "Kevin", last_name: "Chi")
+    expect(student).to be_valid
+  end
+
+  it 'has a first_name' do
+    student = Student.create(first_name: "Kevin")
+    expect(student.first_name).to eq("Kevin")
+  end
+
+  it 'has a last_name' do
+    student = Student.create(last_name: "Kevin")
+    expect(student.last_name).to eq("Kevin")
+  end
+
+
 end
