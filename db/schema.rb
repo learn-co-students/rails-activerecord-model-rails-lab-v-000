b4ef4396e50b1cc3a7f5ef79b938b5dc6a0b3905
Frank McCallion <fkmccallion@gmail.com>
2018-08-28 17:14:00 +0000

    create db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180828171232) do
+
+  create_table "students_tables", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
 end
