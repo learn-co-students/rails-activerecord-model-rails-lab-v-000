09f54012ef1ebcd4f1c60744999828e61283829c
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-05 10:13:42 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "students", force: :cascade do |t|
+  create_table "studets", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
   end
