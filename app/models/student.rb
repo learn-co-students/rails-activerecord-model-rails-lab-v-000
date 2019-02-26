eb46b127c97f93c8a10b311fa91ced24b9bfc0c6
veloblank <veloblank@gmail.com>
2019-02-26 18:12:19 -0500

    Done. Redone.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
-class Student < ActiveRecord::Base
-
-  def to_s
-    first_name + " " + last_name
-  end
-
-=======
-require 'pry'
 class Student < ActiveRecord::Base
 
   def to_s
     self.first_name + " " + self.last_name
   end
 
-
->>>>>>> d1d47fe01e71b0dbc143f674d3666c8117b9f896
 end
--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-class CreateStudents < ActiveRecord::Migration
-
-=======
 class CreateStudents <ActiveRecord::Migration
->>>>>>> d1d47fe01e71b0dbc143f674d3666c8117b9f896
   def change
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
     end
   end
-<<<<<<< HEAD
-
-=======
->>>>>>> d1d47fe01e71b0dbc143f674d3666c8117b9f896
 end
