e4c85b31e63e1759e299814d03056e977a88995e
Christiana Vandermale <christianavdm@gmail.com>
2019-06-20 02:39:34 +0000

    Done.

--- db/migrate/20190620022959_create_students.rb
+++ db/migrate/20190620022959_create_students.rb
@@ -1,4 +1,10 @@
-class Posts < ActiveRecord::Migration[5.0]
+class CreateStudents < ActiveRecord::Migration[5.0]
   def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps null: false
+    end
   end
 end
