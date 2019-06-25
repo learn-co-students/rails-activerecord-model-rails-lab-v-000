5cd1e5ddf5226b5be4e9041b2bc6c9451dbf1cb6
Kat Tochowicz <ktochow1@binghamton.edu>
2019-06-25 14:43:29 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ActiveRecord::Base
 
   def to_s
     @student = Student.create!()
-    "#{self.first_name + self.last_name}"
+    "#{self.first_name}" + " " + "#{self.last_name}"
   end
 
 end
