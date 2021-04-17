ad864c1e569bea2c6dbb98a8e5d94b98cfeeae32
lrh2daizzo <herrera.lrh@gmail.com>
2021-04-17 14:00:48 -0500

    Done.

--- db/migrate/20201217010153_create_students.rb
+++
@@ -1,8 +0,0 @@
-class CreateStudents < ActiveRecord::Migration[5.0]
-  def change
-    create_table :students do |t|
-      t.string :first_name
-      t.string :last_name
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201217010153) do
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
